e3de1c41a8fb54f326371f0a34821947095341a6
RioChx <73mahy@gmail.com>
2025-12-24 10:41:36 +1100

    Downgrade Gradle and Kotlin plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    // We update this to 8.2.0 to be compatible with newer Gradle versions
-    id("com.android.application") version "8.2.0" apply false
-    id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
